8b438c6d68a83985fdd698bc2cb2edb95f573e58
Andrii Androsovych <kozakizdona@gmail.com>
2023-10-18 14:06:09 +0300

    jmcgjc

--- clue.js
+++ clue.js
@@ -9,20 +9,27 @@ class ClueInput{
     }
 
     _onBtnSend(evt){
-       
+        
+    }
+
+    _onChangeInput(evt){
+        console.log( evt.target.value);
     }
 
     _createList (list=['1','2','3']) {
         let listNode = document.createElement('ul');
-        for(const item of list){
+        listNode.classList.add("clue-menu","p-0","m-0","d-block","w-100","rounded-2");
+        for(const itemText of list){
             //create list item
             let listItem = document.createElement("li");
             listItem.classList.add("px-3","py-2");
-            listItem.setAttribute("data-value",item);
-            listItem.innerText = item;
-            listNode.appendChild(listNode);
+            listItem.setAttribute("data-value",itemText);
+            listItem.innerText = itemText;
+            listNode.appendChild(listItem);
             listItem.addEventListener("click",this._onClickEventMenu);
         }
+        let placeToInsert = this.parentNode.querySelector(".clue-container");
+        placeToInsert.appendChild(listNode)
 
         return listNode;
     }
@@ -47,15 +54,20 @@ class ClueInput{
         let btnSend = document.createElement("button");
         btnSend.setAttribute("type","button");
         btnSend.classList.add("clue-send", "m-2", "rounded-2");
+        btnSend.innerText = "Select"
         //button and input wrapper
         let btnInpWrapper = document.createElement("div");
         btnInpWrapper.classList.add("d-flex", "flex-row", "justify-content-start", "align-items-center");
+        //event listener for a button
+        btnSend.addEventListener('click',this._onBtnSend);
+        //when user typing something into the Input
+        textInput.addEventListener("input",this._onChangeInput)
         //asign child elems
-        btnInpWrapper.appendChild(textInput,btnSend);
+        btnInpWrapper.appendChild(textInput);
+         btnInpWrapper.appendChild(btnSend);
         container.appendChild(btnInpWrapper);
         this.parentNode.appendChild(container);
-        //event listener for a button
-        btnSend.addEventListener(this._onBtnSend);
+        
         
     }
 
@@ -72,6 +84,9 @@ class ClueInput{
 }
 
 window.onload=function(){
-    let pNode = document.querySelector(".clue-container");
+    let pNode = document.querySelector(".inserted_clue");
+    let clue = new ClueInput(pNode);
+    clue.createFramework();
+    clue._createList();
     
 }
