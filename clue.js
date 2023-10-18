4d869f293cc19b0199aaa3e1bc7d9d4381abdb01
Andrii Androsovych <kozakizdona@gmail.com>
2023-10-18 15:38:11 +0300

    dgdrhrrdhre

--- clue.js
+++ clue.js
@@ -1,19 +1,61 @@
 class ClueInput{
     
-    constructor(parentNode){
+    constructor(parentNode, list=[  
+                                    "Oliver",
+                                    "Olivia",
+                                    "George",
+                                    "Amelia",
+                                    "Harry",
+                                    "Isla",
+                                    "Jack",
+                                    "Ava",
+                                    "Arthur",
+                                    "Mia",
+                                    "Leo",
+                                    "Sophia",
+                                    "Muhammad",
+                                    "Grace",
+                                    "Noah",
+                                    "Lily",
+                                    "Oscar",
+                                    "Freya",
+                                    "Charlie",
+                                    "Ivy"]){
        this.parentNode = parentNode;
+       this.list=list;
     }
 
     _onClickEventMenu(evt){
-       console.log(evt.target.getAttribute("data-value"));
+       let valueOfItem = evt.target.getAttribute("data-value");
+       this.parentNode.querySelector("input.clue-input").value = valueOfItem;
     }
 
-    _onBtnSend(evt){
+    _onBtnSend (evt) {
         
     }
 
-    _onChangeInput(evt){
-        this._removeList ();
+    _matchingArrayByTemplate(template="a", array=["apple", "bear", "garlic", "ananas", "gam", "12"]) {
+        if(template.length < 1){
+            //when a string is empty - return an empty array
+            return [];
+        }
+        let result = [];
+        let tool= new RegExp(`^${template}\w*`);
+        for (const a of array){
+            if(tool.test(a)) {
+                result.push(a)
+            }
+        }
+        return result;
+    }
+
+    _onChangeInput (evt) {
+        //remove li elems when exists
+        this._removeList();
+        //matching in according to user`s string
+        let matched =  this._matchingArrayByTemplate(evt.target.value, this.list);
+        //create a new <li> items
+        this._createList(matched);
     }
 
     _createList (list=['1','2','3','4','5']) {
@@ -91,6 +133,6 @@ window.onload=function(){
     let pNode = document.querySelector(".inserted_clue");
     let clue = new ClueInput(pNode);
     clue.createFramework();
-    clue._createList();
+   //
     
 }
